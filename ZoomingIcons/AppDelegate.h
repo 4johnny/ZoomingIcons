794c7daa6b3d179050d7936264f851565b07389a
4johnny <4johnny@users.noreply.github.com>
2015-02-19 13:43:58 -0800

    Initial Commit

---
+++ ZoomingIcons/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZoomingIcons
+//
+//  Created by Johnny on 2015-02-19.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZoomingIcons/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZoomingIcons
+//
+//  Created by Johnny on 2015-02-19.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
