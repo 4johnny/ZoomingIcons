af8dfb9d693abb7d7f0582404dcb673f5c775cb0
4johnny <4johnny@users.noreply.github.com>
2015-02-19 23:00:22 -0800

    Add detail view controller

---
+++ ZoomingIcons/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  ZoomingIcons
+//
+//  Created by Johnny on 2015-02-19.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
